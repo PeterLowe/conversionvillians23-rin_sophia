36406e0fb0a05412900ecb79e7db5f4f10d2ad04
(Student  C00299461) Sophia Stanley <C00299461@setu.ie>
2023-11-17 09:46:18 +0000

    Print Menu 2

--- GitLab4/GitLab4/Convertor.cpp
+++ GitLab4/GitLab4/Convertor.cpp
@@ -1,7 +1,23 @@
 // Rin Larkin
 // Sophia Stanley
 
+#include <iostream>
+
 int main()
 {
 	return 0;
+}
+
+char printMenu() // written by Sophia
+{
+	char choice = ' '; // the user's choice
+	std::cout << "Please enter an uppercase letter to convert from left to right\nand lowercase to convert from right to left\n\n" << std::endl;
+
+	std::cout << "[D] Mpg		<--->	L/100km		[d]" << std::endl;
+	std::cout << "[E] Kilograms	<--->	Stone & Pounds	[e]" << std::endl;
+	std::cout << "[F] Celsius	<--->	Fahrenheit	[f]" << std::endl;
+	std::cout << "[Q] Exit		[...]	Exit		[q]" << std::endl;
+	std::cout << "Enter your choice: ";
+	std::cin >> choice;
+	return choice;
 }

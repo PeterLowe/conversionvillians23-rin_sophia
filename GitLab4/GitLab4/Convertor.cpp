8307ff7b7480c3a2c0962514ba16c114cd66fd64
(Student  C00241015) Muirinn Larkin <C00241015@setu.ie>
2023-11-17 09:41:46 +0000

    Print Function

--- GitLab4/GitLab4/Convertor.cpp
+++ GitLab4/GitLab4/Convertor.cpp
@@ -1,7 +1,36 @@
 // Rin Larkin
 // Sophia Stanley
 
+#include <iostream>
+
+char printMenu(); // function declaration
+
 int main()
-{
+{	
+	//Declare Variables
+	char userChoice = ' ';
+
+	userChoice = printMenu(); // function call
+
 	return 0;
+}
+
+char printMenu() // Written by Rin
+{
+	// Declare Variables
+	char choice = ' ';
+
+	// User Input
+	std::cout << "Enter an Upper Case Letter to Convert from Left to Right, and Lower Case to Convert from Right to Left" << std::endl;
+	
+	std::cout << "[A] Degrees <---> Radians [a]" << std::endl;
+	std::cout << "[B] Centimetres <---> Feet & Inches [b]" << std::endl;
+	std::cout << "[C] Litres <---> Gallons [c]" << std::endl;
+
+	std::cout << "[Q] Exit [---] Exit [q]" << std::endl;
+
+	std::cout << "Enter Your Choice" << std::endl;
+	std::cin >> choice;
+
+	return choice;
 }
